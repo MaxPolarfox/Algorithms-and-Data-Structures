7808b690dde84172ef3d23fc97fc4ad4f6c0228f
Maksim Pesetski <maksim.pesetski@gmail.com>
2020-01-06 19:44:40 -0500

    Dijkstra Alghoritm

--- AloritmsAndDataStractures.js
+++ AloritmsAndDataStractures.js
@@ -1498,3 +1498,69 @@ class Graph {
     return false;
   }
 }
+
+/*
+Dijkstra Alghoritm: helps to find the shortest way to one vertex to another using the priority Queue I implemented above
+*/
+class WeightedGraph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(vertex1, vertex2, weight) {
+    this.adjacencyList[vertex1].push({ node: vertex2, weight });
+    this.adjacencyList[vertex2].push({ node: vertex1, weight });
+  }
+
+  Dijkstra(start, finish) {
+    const priorityQueue = new PriorityQueue();
+    const distances = {};
+    const previous = {};
+    let path = [] //to return at end
+    let smallest;
+    //build up initial state
+    for (let vertex in this.adjacencyList) {
+      if (vertex === start) {
+        distances[vertex] = 0;
+        priorityQueue.enqueue(vertex, 0);
+      } else {
+        distances[vertex] = Infinity;
+        priorityQueue.enqueue(vertex, Infinity);
+      }
+      previous[vertex] = null;
+    }
+    // as long as there is something to visit
+    while (priorityQueue.values.length) {
+      smallest = priorityQueue.dequeue().val;
+      if (smallest === finish) {
+        //WE ARE DONE
+        //BUILD UP PATH TO RETURN AT END
+        while (previous[smallest]) {
+          path.push(smallest);
+          smallest = previous[smallest];
+        }
+        break;
+      }
+      if (smallest || distances[smallest] !== Infinity) {
+        for (let neighbor in this.adjacencyList[smallest]) {
+          //find neighboring node
+          let nextNode = this.adjacencyList[smallest][neighbor];
+          //calculate new distance to neighboring node
+          let candidate = distances[smallest] + nextNode.weight;
+          let nextNeighbor = nextNode.node;
+          if (candidate < distances[nextNeighbor]) {
+            //updating new smallest distance to neighbor
+            distances[nextNeighbor] = candidate;
+            //updating previous - How we got to neighbor
+            previous[nextNeighbor] = smallest;
+            //enqueue in priority queue with new priority
+            priorityQueue.enqueue(nextNeighbor, candidate);
+          }
+        }
+      }
+    }
+    return path.concat(smallest).reverse();
+  }
+}
